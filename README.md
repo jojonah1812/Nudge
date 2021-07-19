53966b4f0f1ae5ae5dc597759b37509bd678dd7c
Chad <chadndonahue@gmail.com>
2021-07-19 16:19:26 -0400

    Starter code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Nudge
+# MERN-template
+MERN template
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express"),
+      app = express(),
+      mongoose = require("mongoose"),
+      passport = require("passport"),
+      logger = require("morgan"),
+      apiRoutes = require("./routes"),
+      PORT = process.env.PORT || 3001;
+require("dotenv").config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(logger("dev"));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+app.use(apiRoutes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/myproject", { useNewUrlParser: true });
+
+app.listen(PORT, () => console.log(`The server has started on PORT: ${PORT}`));
