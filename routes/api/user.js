f965c92327c1d6f7bfcd0cbc5c45db7072104656
Jonah <82416656+jojonah1812@users.noreply.github.com>
2021-07-21 12:31:21 -0700

    Controllers.

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 // imports all logic from controller
-const userController = require("../../controllers/UserController");
+const userController = require("../../controllers/userController");
 const passport = require("../../configs/passport");
 
 router.route("/:id").get(userController.getUser);
