9220d15c012920570120ea8cf0566e75ea8ec181
sclo00ps <louginescott@gmail.com>
2021-07-20 17:18:52 -0700

    landing, userprofile, jobposts, jobboard pages added

---
+++ client/src/components/UserprofileForm/index.js
@@ -0,0 +1,90 @@
+import React from "react";
+import "./style.css";
+import Form from 'react-bootstrap/Form';
+
+
+//User Profile Input Fields
+
+<Form>
+  <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+  </Row>
+
+  <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridName">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="First and Last Name" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPhone">
+      <Form.Label>Phone Number</Form.Label>
+      <Form.Control placeholder="phone number" />
+    </Form.Group>
+  </Row>
+
+  <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control placeholder="City" />
+    </Form.Group>
+    
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Label>Zip</Form.Label>
+      <Form.Control placeholder="Zip code" />
+    </Form.Group>
+  </Row>
+
+  <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridStartDate">
+      <Form.Label>Start Date</Form.Label>
+      <Form.Control type="date" placeholder="mm/dd/yyyy" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridSalary">
+      <Form.Label>Salary</Form.Label>
+      <Form.Control placeholder="Salary" />
+    </Form.Group>
+  </Row>
+
+  <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridJobType">
+      <Form.Label>Job Type</Form.Label>
+      <Form.Control placeholder="FT/PT/Contract" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridExperienceLevel">
+      <Form.Label>PExperience </Form.Label>
+      <Form.Control placeholder="Entry/Mid-level/Senior" />
+    </Form.Group>
+  </Row>
+
+  <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridTechnicalEducation">
+      <Form.Label>Tech education</Form.Label>
+      <Form.Control placeholder="Technical Education" />
+    </Form.Group>
+   
+    <Form.Group as={Col} controlId="formGridSchool">
+      <Form.Label>School</Form.Label>
+      <Form.Control placeholder="School Name" />
+    </Form.Group>
+  </Row>
+
+  <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control placeholder="City" />
+    </Form.Group>
+   
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+{/*import React from "react";
 import axios from "axios";
 import { useAuth } from "../../utils/auth/"
 import API from "../../utils/axios/API";
@@ -29,3 +29,107 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
+*/}
+
+import React, { useState, useRef } from "react";
+import { Redirect } from "react-router-dom";
+import { signup, useAuth } from "../../utils/auth/";
+import {
+  AuthenticationContainer,
+  AuthenticationForm,
+} from "../../components/Authentication/";
+// can insert code for signup and login - forms tether to state use useeffect and usestate use as template for landing page
+const loginFields = [
+  { name: "username", type: "text", placeholder: "username" },
+  { name: "password", type: "password", placeholder: "password" },
+];
+const signUpFields = [
+  { name: "username1", type: "text", placeholder: "username" },
+  { name: "password1", type: "password", placeholder: "password" },
+  { name: "email", type: "text", placeholder: "JohnSmith@gmail.com" },
+  { name: "firstName", type: "text", placeholder: "First Name" },
+  { name: "lastName", type: "text", placeholder: "Last Name" },
+];
+
+const Login = () => {
+  const [isLeft, setIsLeft] = useState(false);
+  const [formData, setFormData] = useState();
+  const [currentForm, setCurrentForm] = useState("login");
+  const sliderRef = useRef();
+  const topLayerRef = useRef();
+  const { login, isPending, isLoggedIn, error } = useAuth();
+
+  const handleChange = formObj => {
+    setFormData({ ...formData, ...formObj });
+  };
+
+  const handleClick = e => {
+    e.preventDefault();
+    if (!isLeft) {
+      setIsLeft(true);
+      setFormData();
+      setCurrentForm("signup");
+      sliderRef.current.style.marginLeft = "0";
+      topLayerRef.current.style.marginLeft = "100%";
+      loginFields.forEach(login => document.getElementById(login.name).value = "");
+    }
+
+    if (isLeft) {
+      setIsLeft(false);
+      setFormData();
+      setCurrentForm("login");
+      sliderRef.current.style.marginLeft = "50%";
+      topLayerRef.current.style.marginLeft = "0";
+      signUpFields.forEach(login => document.getElementById(login.name).value = "");
+    }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(currentForm === "login") {
+        login(formData);
+    } else {
+        signup(formData);
+        const { username1, password1 } = formData;
+        login({username: username1, password: password1});
+    }
+  };
+
+  if(isLoggedIn) return <Redirect to="/" />;
+  if(isPending) return <h1>Loading...</h1>;
+
+  return (
+    <>
+      <div id='back'>
+        <div className='backRight'></div>
+        <div className='backLeft'></div>
+      </div>
+      <div id='slideBox' ref={sliderRef}>
+        <div className='topLayer' ref={topLayerRef}>
+          <AuthenticationContainer direction='right'>
+            <AuthenticationForm
+              formData={loginFields}
+              title={"Login"}
+              handleChange={handleChange}
+              formState={formData}
+            />
+            <button onClick={handleClick}>Sign Up</button>
+            <button onClick={handleSubmit}>Login</button>
+          </AuthenticationContainer>
+          <AuthenticationContainer direction='left'>
+            <AuthenticationForm
+              formData={signUpFields}
+              title={"Sign Up"}
+              handleChange={handleChange}
+              formState={formData}
+            />
+            <button onClick={handleSubmit}>Sign Up</button>
+            <button onClick={handleClick}>Login</button>
+          </AuthenticationContainer>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
