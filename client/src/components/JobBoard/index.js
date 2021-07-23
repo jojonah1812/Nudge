f4ddb4f43dba8eceb3188b1936cec6b4d244a758
Jonah <82416656+jojonah1812@users.noreply.github.com>
2021-07-23 13:47:31 -0700

    Job board Page input code.

--- client/src/components/JobBoard/index.js
+++ client/src/components/JobBoard/index.js
@@ -1,40 +1,35 @@
 import React from "react";
 import "./style.css";
-import Col from "react-bootstrap/Col";
-import Container from 'react-bootstrap/Container'
-import Col from "react-bootstrap/Col";
+import { ListItem } from "react-boostrap/ListItem";
+import { Row, Col } from "react-bootstrap/Col";
+import { Row } from "react-bootstrap/Row";
 // import ListGroup from 'react-bootstrap/ListGroup'
 // want to list the job headers 
-function Jobs() {
-    return (
-        <div class="cardContainer">
-         <Grid>
-              <Container>
-                <Row>
-                  <Col xs={6} md={4}>sm=true</Col>
-                  <Col xs={6} md={4}>sm=true</Col>
-                  <Col xs={6} md={4}>sm=true</Col>
-                
-                  <Col xs={6} md={4}>sm=true</Col>
-                  <Col xs={6} md={4}>sm=true</Col>
-                  <Col xs={6} md={4}>sm=true</Col>
-                
-                  <Col xs={6} md={4}>sm=true</Col>
-                  <Col xs={6} md={4}>sm=true</Col>
-                  <Col xs={6} md={4}>sm=true</Col>
-                
-                  <Col xs={6} md={4}>sm=true</Col>
-                  <Col xs={6} md={4}>sm=true</Col>
-                  <Col xs={6} md={4}>sm=true</Col>           
-              
-                  <Col xs={6} md={4}>sm=true</Col>
-                  <Col xs={6} md={4}>sm=true</Col>
-                  <Col xs={6} md={4}>sm=true</Col>
-                </Row>
-              </Container>
-          </Grid>
-        </div>   
-    )
-};
 
+//import "./style.css";
+function Jobs({ position }) {
+  return (
+    <ListItem>
+      <Row className="flex-wrap-reverse">
+        <Col size="md-8">
+          <h3 className="font-italic">{position}</h3>
+          {subtitle && <h5 className="font-italic">{position}</h5>}
+        </Col>
+        <Col size="md-4">
+          <div className="btn-container">
+            <a
+              className="btn btn-light"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={link}
+            >
+              View
+            </a>
+            {/* <Button /> */}
+          </div>
+        </Col>
+      </Row>
+    </ListItem>
+  );
+}
 export default Jobs;
--- client/src/pages/JobBoardPage/JobBoardPage.js
+++ client/src/pages/JobBoardPage/JobBoardPage.js
@@ -1,41 +1,87 @@
 import React, { Component } from "react";
-// import Card from "../components/Card";
+import Jumbotron from "../components/Jumbotron";
+import Card from "../components/Card";
 //import Footer from "../components/Footer";
 //import Navbar from "../components/Navbar";
 //import Hero from "../components/Hero";
-import API from "../../utils/axios/API";
+//import API from "../../utils/axios/API";
 //import { Col, Row, Container } from "../components/Grid";
 //import { List } from "../components/List";
-
-class JobBoardPage extends Component {
-  state = {
-    jobs: []
+//import { Col, Row, Container } from "react-bootstrap/Grid";
+import { List } from "../components/List";
+function JobBoardPage() {
+  const [JobBoard, setJobBoard] = useState(0);
+  getJobs = () => {
+    API.getJobs()
+      .then((res) =>
+        setJobBoard({
+          JobBoard: res.data,
+        })
+      )
+      .catch(() =>
+        setJobBoard({
+          JobBoard: [],
+          message: "No New Jobs Found, Try a Different Query",
+        })
+      );
   };
-
-  // componentDidMount() {
-  //   this.getSavedJobs();
-  // }
-
-  // getJobsBoard = () => {
-  //   API.getSavedJobs()
-  //     .then(res =>
-  //       this.setState({
-  //         jobs: res.data
-  //       })
-  //     )
-  //     .catch(err => console.log(err));
-  // };
-
- /* handleBookDelete = id => {
-    API.deleteJob(id).then(res => this.get());
-  };
- */ 
-  render() {
-    return (
-      <div>JOB BOARD</div>
-    );
-    }
-  }
-
+  return (
+    <Container>
+      <Row>
+        <Col size="md-12">
+          <Jumbotron>
+            <h1 className="text-center">
+              <strong>(React) Google Books Search</strong>
+            </h1>
+            <h2 className="text-center">
+              Search for and Save Books of Interest.
+            </h2>
+          </Jumbotron>
+        </Col>
+        <Col size="md-12">
+          <Card title="Book Search" icon="far fa-book">
+            <Form
+              handleInputChange={this.handleInputChange}
+              handleFormSubmit={this.handleFormSubmit}
+              q={this.state.q}
+            />
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col size="md-12">
+          <Card title="Results">
+            {this.state.books.length ? (
+              <List>
+                {this.state.books.map((book) => (
+                  <Book
+                    key={book.id}
+                    title={book.volumeInfo.title}
+                    subtitle={book.volumeInfo.subtitle}
+                    link={book.volumeInfo.infoLink}
+                    authors={book.volumeInfo.authors.join(", ")}
+                    description={book.volumeInfo.description}
+                    image={book.volumeInfo.imageLinks.thumbnail}
+                    Button={() => (
+                      <button
+                        onClick={() => this.handleBookSave(book.id)}
+                        className="btn btn-primary ml-2"
+                      >
+                        Save
+                      </button>
+                    )}
+                  />
+                ))}
+              </List>
+            ) : (
+              <h2 className="text-center">{this.state.message}</h2>
+            )}
+          </Card>
+        </Col>
+      </Row>
+      <Footer />
+    </Container>
+  );
+}
 
 export default JobBoardPage;
