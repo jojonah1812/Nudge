7216a153c0da7ffb0796a21c6cfb670ac38e598c
Jonah <82416656+jojonah1812@users.noreply.github.com>
2021-07-26 13:06:58 -0700

    Forms

--- client/src/pages/JobPostsPage/JobPostsPage.js
+++ client/src/pages/JobPostsPage/JobPostsPage.js
@@ -7,49 +7,48 @@ class JobPostsPage extends Component {
 
     render() {
         return (
-            <div>
-                <div>JOB POSTS</div>
+          <div>
+            <div className="cardTitle">POST A JOB</div>
 
-
-                <Form>
-                    <Form.Group className="mb-3" controlId="formGridPosition">
-                        <Form.Label>Position</Form.Label>
-                        <Form.Control placeholder="Postiion Name" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formGridDatePosted">
-                        <Form.Label>Date Posted</Form.Label>
-                        <Form.Control type="date" placeholder="Date Posted" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formGridPositionID">
-                        <Form.Label>Position ID</Form.Label>
-                        <Form.Control placeholder="Position ID" />
-                    </Form.Group>
-                    <Form.Group  controlId="formGridDescription">
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control placeholder="Description" />
-                    </Form.Group>
-                    <Form.Group  controlId="formGridLanguages">
-                        <Form.Label>Languages</Form.Label>
-                        <Form.Control placeholder="Coding Languages" />
-                    </Form.Group>
-                    <Form.Group  controlId="formGridExperience">
-                        <Form.Label>Experience</Form.Label>
-                        <Form.Control placeholder="Experience" />
-                    </Form.Group>
-                    <Form.Group  controlId="formGridSalary">
-                        <Form.Label>Salary</Form.Label>
-                        <Form.Control placeholder="Salary Range" />
-                    </Form.Group>
-                    <Form.Group  controlId="formGridAuthor">
-                        <Form.Label>Author</Form.Label>
-                        <Form.Control placeholder="Author of the Job Posting" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </div>
-        )
+            <Form className="card">
+              <Form.Group className="mb-3" controlId="formGridPosition">
+                <Form.Label>Position</Form.Label>
+                <Form.Control placeholder="Position name" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGridDatePosted">
+                <Form.Label>Date Posted</Form.Label>
+                <Form.Control type="date" placeholder="Date Posted" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGridPositionID">
+                <Form.Label>Position ID</Form.Label>
+                <Form.Control placeholder="Position ID" />
+              </Form.Group>
+              <Form.Group controlId="formGridDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control placeholder="Description" />
+              </Form.Group>
+              <Form.Group controlId="formGridLanguages">
+                <Form.Label>Languages</Form.Label>
+                <Form.Control placeholder="Coding languages required" />
+              </Form.Group>
+              <Form.Group controlId="formGridExperience">
+                <Form.Label>Experience</Form.Label>
+                <Form.Control placeholder="Years of experience" />
+              </Form.Group>
+              <Form.Group controlId="formGridSalary">
+                <Form.Label>Salary</Form.Label>
+                <Form.Control placeholder="Salary range" />
+              </Form.Group>
+              <Form.Group controlId="formGridAuthor">
+                <Form.Label>Author</Form.Label>
+                <Form.Control placeholder="Author of this job posting" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </div>
+        );
 
     }
 
--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -4,110 +4,115 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-
 class LandingPage extends Component {
   render() {
     return (
       <div>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Remember me" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-
-        <Form>
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridEmail">
-              <Form.Label>Email</Form.Label>
+        <div>
+          <div className="cardTitle">Log In</div>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridPassword">
+            <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" />
             </Form.Group>
-          </Row>
 
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridName">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="text" placeholder="First and Last Name" />
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Remember me" />
             </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
 
-            <Form.Group as={Col} controlId="formGridPhone">
-              <Form.Label>Phone Number</Form.Label>
-              <Form.Control placeholder="phone number" />
-            </Form.Group>
-          </Row>
+        <div>
+          <div className="cardTitle">Signup</div>
+          <Form>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
 
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridCity">
-              <Form.Label>City</Form.Label>
-              <Form.Control placeholder="City" />
-            </Form.Group>
+              <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Row>
 
-            <Form.Group as={Col} controlId="formGridZip">
-              <Form.Label>Zip</Form.Label>
-              <Form.Control placeholder="Zip code" />
-            </Form.Group>
-          </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridName">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="text" placeholder="First and Last Name" />
+              </Form.Group>
 
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridStartDate">
-              <Form.Label>Start Date</Form.Label>
-              <Form.Control type="date" placeholder="mm/dd/yyyy" />
-            </Form.Group>
+              <Form.Group as={Col} controlId="formGridPhone">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control placeholder="phone number" />
+              </Form.Group>
+            </Row>
 
-            <Form.Group as={Col} controlId="formGridSalary">
-              <Form.Label>Salary</Form.Label>
-              <Form.Control placeholder="Salary" />
-            </Form.Group>
-          </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control placeholder="City" />
+              </Form.Group>
 
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridJobType">
-              <Form.Label>Job Type</Form.Label>
-              <Form.Control placeholder="FT/PT/Contract" />
-            </Form.Group>
+              <Form.Group as={Col} controlId="formGridZip">
+                <Form.Label>Zip</Form.Label>
+                <Form.Control placeholder="Zip code" />
+              </Form.Group>
+            </Row>
 
-            <Form.Group as={Col} controlId="formGridExperienceLevel">
-              <Form.Label>Experience </Form.Label>
-              <Form.Control placeholder="Entry/Mid-level/Senior" />
-            </Form.Group>
-          </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridStartDate">
+                <Form.Label>Start Date</Form.Label>
+                <Form.Control type="date" placeholder="mm/dd/yyyy" />
+              </Form.Group>
 
-          <Row className="mb-3">
-            <Form.Group as={Col} controlId="formGridTechnicalEducation">
-              <Form.Label>Tech education</Form.Label>
-              <Form.Control placeholder="Technical Education" />
-            </Form.Group>
+              <Form.Group as={Col} controlId="formGridSalary">
+                <Form.Label>Salary</Form.Label>
+                <Form.Control placeholder="Salary" />
+              </Form.Group>
+            </Row>
 
-            <Form.Group as={Col} controlId="formGridSchool">
-              <Form.Label>School</Form.Label>
-              <Form.Control placeholder="School Name" />
-            </Form.Group>
-          </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridJobType">
+                <Form.Label>Job Type</Form.Label>
+                <Form.Control placeholder="FT/PT/Contract" />
+              </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+              <Form.Group as={Col} controlId="formGridExperienceLevel">
+                <Form.Label>Experience </Form.Label>
+                <Form.Control placeholder="Entry/Mid-level/Senior" />
+              </Form.Group>
+            </Row>
+
+            <Row className="mb-3">
+              <Form.Group as={Col} controlId="formGridTechnicalEducation">
+                <Form.Label>Tech education</Form.Label>
+                <Form.Control placeholder="Technical Education" />
+              </Form.Group>
+
+              <Form.Group as={Col} controlId="formGridSchool">
+                <Form.Label>School</Form.Label>
+                <Form.Control placeholder="School Name" />
+              </Form.Group>
+            </Row>
+
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
     );
   }
