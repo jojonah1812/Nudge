783b910eb14478ec1f5ddb8da9336b5bbbbe92ff
sclo00ps <louginescott@gmail.com>
2021-07-28 18:52:35 -0700

    removed Component from React import statment for Navbar and UserList components

--- client/src/components/UsersList/index.js
+++ client/src/components/UsersList/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 // import { ListItem } from "../List";
 // import { Row, Col } from "../../Grid";
 import Row from "react-bootstrap/Row";
--- client/src/pages/JobPostsPage/JobPostsPage.js
+++ client/src/pages/JobPostsPage/JobPostsPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 //import Jumbotron from "react-bootstrap/Jumbotron";
 //import Card from "react-bootstrap/Card";
 //import Posts from "../../components/Posts";
