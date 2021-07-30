ec11e40a70c94cae3809b8f8b093a24ae85e006b
Jonah <82416656+jojonah1812@users.noreply.github.com>
2021-07-30 13:21:57 -0700

    Added .env file.

---
+++ scripts/seedDB.js
@@ -0,0 +1,7 @@
+// const { Mongoose } = require("mongoose");
+
+// require("dotenv").config();
+// Mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//     useNewUrlParser: true,
+
+// }
